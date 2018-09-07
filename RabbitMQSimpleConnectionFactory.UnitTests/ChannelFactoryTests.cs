408eab9d2e4d7813c43a49c54f9485f0a0dcb06d
Thaise Dantas <tdantas@stone.com.br>
2018-09-07 20:35:37 -0300

    Change unitTest from ConnectionPoolTests to ChannelFactoryTest

---
+++ RabbitMQSimpleConnectionFactory.UnitTests/ChannelFactoryTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using RabbitMQSimpleConnectionFactory.Entity;
+using RabbitMQSimpleConnectionFactory.Library;
+using Xunit;
+
+namespace RabbitMQSimpleConnectionFactory.UnitTests
+{
+    public class ChannelFactoryTests
+    {
+        [Fact]
+        public void GivenGetChannel_WhenNotSetConnectionSetting_ThenConnectionSettingUseDefault()
+        {
+            var connectionSetting = new ConnectionSetting();
+
+            var connectionSettingDefault = new ConnectionSetting
+            {
+                HostName = "localhost",
+                VirtualHost = "/",
+                UserName = "guest",
+                Password = "guest",
+                Port = 5672
+            };
+
+            var channelFactory = new ChannelFactory();
+
+            channelFactory.Create(connectionSetting);
+
+            connectionSetting.Should().BeEquivalentTo(connectionSettingDefault);
+        }
+    }
+}
