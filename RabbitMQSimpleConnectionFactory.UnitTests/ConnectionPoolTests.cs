d8f3fdcc27f66d80f0b21973ddeabb2adf927cdd
Thaise Dantas <tdantas@stone.com.br>
2018-09-07 21:14:02 -0300

    Test for connection pool number

--- RabbitMQSimpleConnectionFactory.UnitTests/ConnectionPoolTests.cs
+++ RabbitMQSimpleConnectionFactory.UnitTests/ConnectionPoolTests.cs
@@ -1,10 +1,8 @@
-﻿using AutoFixture;
-using FluentAssertions;
-using Moq;
+﻿using Moq;
 using RabbitMQ.Client;
-using RabbitMQ.Client.Framing.Impl;
 using RabbitMQSimpleConnectionFactory.Entity;
 using RabbitMQSimpleConnectionFactory.Library;
+using System;
 using Xunit;
 
 
@@ -12,6 +10,24 @@ namespace RabbitMQSimpleConnectionFactory.UnitTests
 {
     public class ConnectionPoolTests
     {
+        private readonly Mock<IChannelFactory> _mockChannelFactory = new Mock<IChannelFactory>();
+        private readonly Mock<IModel> _mockModel = new Mock<IModel>();
 
+        [Fact]
+        public void GivenTryBuildAPooOfConnections_WhenTheNumberOfConnectionsIsLessThan1_ThenThrowArgumentOutOfRangeException()
+        {
+            const int poolSize = 0;
+            var connectionSetting = new ConnectionSetting();
+
+            _mockChannelFactory.Setup(s => s.Create(It.IsAny<ConnectionSetting>(),
+                    It.IsAny<bool>(),
+                    It.IsAny<ushort>(),
+                    It.IsAny<uint>(),
+                    It.IsAny<ushort>(),
+                    It.IsAny<bool>()))
+                .Returns(_mockModel.Object);
+           
+            Assert.Throws<ArgumentOutOfRangeException>(() => new ConnectionPool(poolSize, connectionSetting, _mockChannelFactory.Object));
+        }
     }
 }
