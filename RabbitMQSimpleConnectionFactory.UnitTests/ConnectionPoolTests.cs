a91e945125c9818036012a7ca49587c32fe289cf
Thaise Dantas <tdantas@stone.com.br>
2018-09-08 00:10:45 -0300

    Tests to channel and connections

--- RabbitMQSimpleConnectionFactory.UnitTests/ConnectionPoolTests.cs
+++ RabbitMQSimpleConnectionFactory.UnitTests/ConnectionPoolTests.cs
@@ -5,19 +5,18 @@ using RabbitMQSimpleConnectionFactory.Library;
 using System;
 using Xunit;
 
-
 namespace RabbitMQSimpleConnectionFactory.UnitTests
 {
     public class ConnectionPoolTests
     {
         private readonly Mock<IChannelFactory> _mockChannelFactory = new Mock<IChannelFactory>();
         private readonly Mock<IModel> _mockModel = new Mock<IModel>();
+        private readonly ConnectionSetting _connectionSetting = new ConnectionSetting();
 
         [Fact]
-        public void GivenTryBuildAPooOfConnections_WhenTheNumberOfConnectionsIsLessThan1_ThenThrowArgumentOutOfRangeException()
+        public void GivenTryBuildAPoolOfConnections_WhenTheNumberOfConnectionsIsLessThan1_ThenThrowArgumentOutOfRangeException()
         {
             const int poolSize = 0;
-            var connectionSetting = new ConnectionSetting();
 
             _mockChannelFactory.Setup(s => s.Create(It.IsAny<ConnectionSetting>(),
                     It.IsAny<bool>(),
@@ -27,7 +26,26 @@ namespace RabbitMQSimpleConnectionFactory.UnitTests
                     It.IsAny<bool>()))
                 .Returns(_mockModel.Object);
            
-            Assert.Throws<ArgumentOutOfRangeException>(() => new ConnectionPool(poolSize, connectionSetting, _mockChannelFactory.Object));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new ConnectionPool(poolSize, _connectionSetting, _mockChannelFactory.Object));
+        }
+        
+        [Fact]
+        public void GivenGetChannel_WhenConnectionPoolIsValid_ThenReturnAModelOfConnection()
+        {
+            const int poolSize = 10;
+
+            _mockChannelFactory.Setup(s => s.Create(It.IsAny<ConnectionSetting>(),
+                    It.IsAny<bool>(),
+                    It.IsAny<ushort>(),
+                    It.IsAny<uint>(),
+                    It.IsAny<ushort>(),
+                    It.IsAny<bool>()))
+                .Returns(_mockModel.Object);
+
+            var connectionPool = new ConnectionPool(poolSize, _connectionSetting, _mockChannelFactory.Object);
+            var channel = connectionPool.GetChannel();
+
+            Assert.IsAssignableFrom<IModel>(channel);
         }
     }
 }
