c7d0cbb1a4f0025ec9a6e069a077eace7480a9cf
Matheus André Riboli <mriboli@mundipagg.com>
2018-12-04 15:51:21 -0200

    Updates ChannelFactory to accept clientProvidedName parameter.

--- RabbitMQSimpleConnectionFactory/Library/ConnectionPool.cs
+++ RabbitMQSimpleConnectionFactory/Library/ConnectionPool.cs
@@ -1,25 +1,39 @@
 ﻿using RabbitMQ.Client;
 using RabbitMQSimpleConnectionFactory.Entity;
+using System;
 using System.Collections.Concurrent;
 
-namespace RabbitMQSimpleConnectionFactory.Library {
+namespace RabbitMQSimpleConnectionFactory.Library
+{
 
-    public class ConnectionPool {
+    public class ConnectionPool
+    {
         private static ConcurrentDictionary<int, IModel> _pool;
         private static int _index;
         private static int _size;
         private static readonly object Sync = new object();
         private static ChannelFactory _channelFactory;
 
-        public ConnectionPool(int size, ConnectionSetting connectionSetting) {
+        public ConnectionPool(int size, ConnectionSetting connectionSetting)
+        {
             _size = size;
 
-            _channelFactory = new ChannelFactory(connectionSetting);
+            _channelFactory = new ChannelFactory(connectionSetting) ?? throw new ArgumentNullException(nameof(connectionSetting));
             Init(size);
         }
 
-        public IModel GetChannel() {
-            lock (Sync) {
+        public ConnectionPool(int size, ChannelFactory channelFactory)
+        {
+            _size = size;
+
+            _channelFactory = channelFactory ?? throw new ArgumentNullException(nameof(channelFactory));
+            Init(size);
+        }
+
+        public IModel GetChannel()
+        {
+            lock (Sync)
+            {
                 _index = (_index < _size) ? _index++ : _index = 0;
             }
 
@@ -27,21 +41,27 @@ namespace RabbitMQSimpleConnectionFactory.Library {
             return model;
         }
 
-        private void Close() {
-            for (var position = 0; position < _size; position++) {
-                if (_pool[_index].IsOpen) {
+        private void Close()
+        {
+            for (var position = 0; position < _size; position++)
+            {
+                if (_pool[_index].IsOpen)
+                {
                     _pool[_index].Close();
                 }
             }
         }
 
-        private void Init(int size) {
-            lock (Sync) {
+        private void Init(int size)
+        {
+            lock (Sync)
+            {
                 if (_pool != null) return;
 
                 _pool = new ConcurrentDictionary<int, IModel>();
 
-                for (var position = 0; position < size; position++) {
+                for (var position = 0; position < size; position++)
+                {
                     _pool.TryAdd(position, ConnectionPool._channelFactory.Create());
                 }
             }
